0d9469ecfef8b7f5f3517832d3400fd5cf8ad398
TTYeh <35258945+TTYeh@users.noreply.github.com>
2023-02-02 01:23:55 +0800

    git init

---
+++ Eddy_LeetCode169_PracticeNote/Best Time to Buy and Sell Stock/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] arrayQuestion = new []{1,4,7,6,5};
+Console.WriteLine("Hello, World!");
+
+
+public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        // 微分抓區間，區間比大小
+        for (int i = 0; i < prices.Length; i++)
+        { 
+        
+        }
+
+        //int localPriceChange = 0;
+        //int localMaxDayPrice = 0;
+        //int localMinDayPrice = 0;
+        //foreach (var dayPrice in prices)
+        //{
+        //    dayPrice
+        //    if (dayPrice > localMaxDayPrice) localMaxDayPrice = dayPrice;
+        //    if (dayPrice < localMaxDayPrice) localMaxDayPrice = dayPrice;
+        //}
+
+
+        return
+    }
+}
